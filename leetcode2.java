55fc18902e59c60afda64bf7f500df2fc8ec3fd8
Mohd Ajlal <ajlal00786@gmail.com>
2024-02-12 03:05:29 +0530

    leetcode2

---
+++ leetcode2.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+      ListNode dummy = new ListNode(0);
+      ListNode curr = dummy;
+      int carry = 0;
+  
+      while (l1 != null || l2 != null || carry > 0) {
+        if (l1 != null) {
+          carry += l1.val;
+          l1 = l1.next;
+        }
+        if (l2 != null) {
+          carry += l2.val;
+          l2 = l2.next;
+        }
+        curr.next = new ListNode(carry % 10);
+        carry /= 10;
+        curr = curr.next;
+      }
+  
+      return dummy.next;
+    }
+  }
